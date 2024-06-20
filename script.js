3d6b293f389e9206992f4d745602c146a13f6461
Sanjaymuli <davidkioko495@gmail.com>
2024-06-20 11:11:04 +0300

    created a Portfolio

---
+++ script.js
@@ -0,0 +1,59 @@
+let menuIcon = document.querySelector("menu-Icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick= () => {
+	menuIcon.classList.toggle("bx-x");
+	navbar.classList.toggle("active");
+}
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () =>{
+	sections.forEach((sec) =>{
+		let top = window.scrollY;
+		let offset = sec.offsetTop - 100;
+		let height = sec.offsetHeight;
+		let id = sec.getAttribute("id");
+
+	if(top >=offset && top <offset + height){
+		navLinks.forEach((links)=>{
+			links.classList.remove("active");
+			document.querySelector("header nav[href*=" + id + "}")
+			.classList.add("active");
+		});
+	  }
+  });
+	let header = document.querySelector("header");
+
+	header.classList.toggle("sticky", window.screenY >100);
+
+	menuIcon.classList.remove("bx-x");
+	navbar.classList.remove("active");
+}
+
+// ScrollReveal
+
+ScrollReveal({
+	reset:true,
+	distance: '80px',
+	duration:2000,
+	delay:200
+
+});
+
+ScrollReveal().reveal(".home-content, heading', .about-content, .progress i, .progress span,", { origin: 'top'});
+ScrollReveal().reveal(".about-img, .project-container", { origin: 'bottom'});
+ScrollReveal().reveal(".progress h3, .bar span",  { origin: 'left'});
+
+
+const typed = new Typed('.text',{
+	strings:['Frontend Developer', 'YouTuber', 'Frontend Designer'],
+	typespeed: 90,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true,
+});
+
+
+
